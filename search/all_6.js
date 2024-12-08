a37a801eb7e161e78925feb3d9eb1ec80bfda944
ChristophKarlHeck <ChristophKarlHeck@users.noreply.github.com>
2024-12-08 12:40:25 +0000

    deploy: b163d3c1d954bbc8ad1e37f6f4e1a8a2bff01919

--- search/all_6.js
+++ search/all_6.js
@@ -11,7 +11,7 @@ var searchData=
   ['gain_5f7_252',['Gain_7',['../namespaceAd7124.html#abd599f3b5c3ec9a3dc7f9a47aaf18a9faba7ef6cfe26be44d7bce1fa9ed305a31',1,'Ad7124']]],
   ['get_5fanalog_5finputs_253',['get_analog_inputs',['../Conversion_8h.html#a5f74d7e73a494e1ab2540f4b6ca0f1e9',1,'get_analog_inputs(std::vector&lt; std::array&lt; uint8_t, 3 &gt;&gt; byte_inputs, int databits, float vref, float gain):&#160;Conversion.cpp'],['../Conversion_8cpp.html#a5f74d7e73a494e1ab2540f4b6ca0f1e9',1,'get_analog_inputs(std::vector&lt; std::array&lt; uint8_t, 3 &gt;&gt; byte_inputs, int databits, float vref, float gain):&#160;Conversion.cpp']]],
   ['get_5finput_5fmodel_5fvalues_5ffrom_5fadc_254',['get_input_model_values_from_adc',['../main_8cpp.html#a1fdec6782dc49c41f278f43cc64787b2',1,'main.cpp']]],
-  ['getinstance_255',['getInstance',['../classReadingQueue.html#af0cc99c28c70cae8aef2cd3dfb8dd052',1,'ReadingQueue::getInstance()'],['../classSerialMailSender.html#a06b40a2ebd55157a561d9823bad1a5d0',1,'SerialMailSender::getInstance()'],['../classAD7124.html#ae8a580df50d9321e52b0d262b1d36006',1,'AD7124::getInstance()']]],
+  ['getinstance_255',['getInstance',['../classReadingQueue.html#af0cc99c28c70cae8aef2cd3dfb8dd052',1,'ReadingQueue::getInstance()'],['../classSerialMailSender.html#a06b40a2ebd55157a561d9823bad1a5d0',1,'SerialMailSender::getInstance()'],['../classAD7124.html#a02acf146ca56efdee458971bf56c9ff3',1,'AD7124::getInstance()']]],
   ['getserialmail_256',['GetSerialMail',['../namespaceSerialMail.html#aa8be79341da5d5892f8a685bc42f57f9',1,'SerialMail']]],
   ['getsizeprefixedserialmail_257',['GetSizePrefixedSerialMail',['../namespaceSerialMail.html#ae7565e5358c4c94276f0e938ff1147e9',1,'SerialMail']]]
 ];
--- search/functions_3.js
+++ search/functions_3.js
@@ -2,7 +2,7 @@ var searchData=
 [
   ['get_5fanalog_5finputs_357',['get_analog_inputs',['../Conversion_8cpp.html#a5f74d7e73a494e1ab2540f4b6ca0f1e9',1,'get_analog_inputs(std::vector&lt; std::array&lt; uint8_t, 3 &gt;&gt; byte_inputs, int databits, float vref, float gain):&#160;Conversion.cpp'],['../Conversion_8h.html#a5f74d7e73a494e1ab2540f4b6ca0f1e9',1,'get_analog_inputs(std::vector&lt; std::array&lt; uint8_t, 3 &gt;&gt; byte_inputs, int databits, float vref, float gain):&#160;Conversion.cpp']]],
   ['get_5finput_5fmodel_5fvalues_5ffrom_5fadc_358',['get_input_model_values_from_adc',['../main_8cpp.html#a1fdec6782dc49c41f278f43cc64787b2',1,'main.cpp']]],
-  ['getinstance_359',['getInstance',['../classAD7124.html#ae8a580df50d9321e52b0d262b1d36006',1,'AD7124::getInstance()'],['../classReadingQueue.html#af0cc99c28c70cae8aef2cd3dfb8dd052',1,'ReadingQueue::getInstance()'],['../classSerialMailSender.html#a06b40a2ebd55157a561d9823bad1a5d0',1,'SerialMailSender::getInstance()']]],
+  ['getinstance_359',['getInstance',['../classAD7124.html#a02acf146ca56efdee458971bf56c9ff3',1,'AD7124::getInstance()'],['../classReadingQueue.html#af0cc99c28c70cae8aef2cd3dfb8dd052',1,'ReadingQueue::getInstance()'],['../classSerialMailSender.html#a06b40a2ebd55157a561d9823bad1a5d0',1,'SerialMailSender::getInstance()']]],
   ['getserialmail_360',['GetSerialMail',['../namespaceSerialMail.html#aa8be79341da5d5892f8a685bc42f57f9',1,'SerialMail']]],
   ['getsizeprefixedserialmail_361',['GetSizePrefixedSerialMail',['../namespaceSerialMail.html#ae7565e5358c4c94276f0e938ff1147e9',1,'SerialMail']]]
 ];
