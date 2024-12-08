ee3e90e3ce6f783aca9a187a1d17eee55eb3485f
ChristophKarlHeck <ChristophKarlHeck@users.noreply.github.com>
2024-12-08 13:14:00 +0000

    deploy: cf3853b8d9a091af184b91112d7b97e9772d01d2

--- search/all_c.js
+++ search/all_c.js
@@ -2,6 +2,6 @@ var searchData=
 [
   ['phyto_20node_286',['Phyto Node',['../index.html',1,'']]],
   ['prev_5fidle_5ftime_287',['prev_idle_time',['../namespacembed__lib.html#a8d16dc38bf192209ca2159aa0783847c',1,'mbed_lib']]],
-  ['print_5fcpu_5fstats_288',['print_cpu_stats',['../namespacembed__lib.html#a670f470c578cc696032d9cc28a13918a',1,'mbed_lib']]],
-  ['print_5fmemory_5fusage_289',['print_memory_usage',['../namespacembed__lib.html#ab3bcf8c0340ccc24a96e38ffb0fd2b40',1,'mbed_lib']]]
+  ['print_5fcpu_5fstats_288',['print_cpu_stats',['../namespacembed__lib.html#a670f470c578cc696032d9cc28a13918a',1,'mbed_lib::print_cpu_stats()'],['../MbedStatsWrapper_8h.html#ac24ef55bcb90642393fdfe49e97561ad',1,'print_cpu_stats():&#160;MbedStatsWrapper.h']]],
+  ['print_5fmemory_5fusage_289',['print_memory_usage',['../namespacembed__lib.html#ab3bcf8c0340ccc24a96e38ffb0fd2b40',1,'mbed_lib::print_memory_usage()'],['../MbedStatsWrapper_8h.html#af534de48ff7a22924644e4e2eb4ba00a',1,'print_memory_usage():&#160;MbedStatsWrapper.h']]]
 ];
--- search/functions_8.js
+++ search/functions_8.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['print_5fcpu_5fstats_366',['print_cpu_stats',['../namespacembed__lib.html#a670f470c578cc696032d9cc28a13918a',1,'mbed_lib']]],
-  ['print_5fmemory_5fusage_367',['print_memory_usage',['../namespacembed__lib.html#ab3bcf8c0340ccc24a96e38ffb0fd2b40',1,'mbed_lib']]]
+  ['print_5fcpu_5fstats_366',['print_cpu_stats',['../namespacembed__lib.html#a670f470c578cc696032d9cc28a13918a',1,'mbed_lib::print_cpu_stats()'],['../MbedStatsWrapper_8h.html#ac24ef55bcb90642393fdfe49e97561ad',1,'print_cpu_stats():&#160;MbedStatsWrapper.h']]],
+  ['print_5fmemory_5fusage_367',['print_memory_usage',['../namespacembed__lib.html#ab3bcf8c0340ccc24a96e38ffb0fd2b40',1,'mbed_lib::print_memory_usage()'],['../MbedStatsWrapper_8h.html#af534de48ff7a22924644e4e2eb4ba00a',1,'print_memory_usage():&#160;MbedStatsWrapper.h']]]
 ];
